953e6ab8a9c1c15210e374ffac65ac9ca5decde7
Gopal Bhojwani <gopal@gopals-mbp.net.sahm.ae>
2017-07-04 15:40:14 +0400

    Initial Commit

---
+++ social-media/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  social-media
+//
+//  Created by Gopal Bhojwani on 7/4/17.
+//  Copyright © 2017 Gopal Bhojwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
